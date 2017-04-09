866f8e2e33393fdb19398b1140e0b53ef28807eb
Sabiul Islam <234sabiul@gmail.com>
2017-04-09 11:01:15 +0600

    changes on __init__.py
    checking if the database already exist

--- __init__.py
+++ __init__.py
@@ -1,10 +1,17 @@
 import mysql.connector
+from mysql.connector import errorcode
 
 __author__ = 'Rashed'
 
 connection = mysql.connector.connect(user='root', password='', host='localhost')
 mycursor = connection.cursor()
 
+try:
+    mycursor.execute("CREATE DATABASE movieRatings  DEFAULT CHARACTER SET 'utf8'")
+except mysql.connector.Error as err:
+        if err.errno == errorcode.ER_DB_CREATE_EXISTS:
+            print("Database already exists.")
+        else:
+            print(err.msg)
 
-#mycursor.execute("CREATE DATABASE movieRatings  DEFAULT CHARACTER SET 'utf8'")
 mycursor.execute("USE movieRatings")
